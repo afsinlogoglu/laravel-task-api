d15119f84be6fca50b310e7302523579d59670c1
Afsin Logoglu <muhammedafsin@hotmail.com>
2025-08-19 03:40:27 +0300

    db connection fix

--- routes/web.php
+++ routes/web.php
@@ -24,14 +24,14 @@ Route::prefix('api')->group(function () {
         // Team routes
         Route::post('/teams', [TeamController::class, 'store']);
         Route::get('/teams', [TeamController::class, 'index']);
-        Route::post('/teams/{id}/members', [TeamController::class, 'addMember']);
-        Route::delete('/teams/{id}/members/{userId}', [TeamController::class, 'removeMember']);
+        Route::post('/teams/{id}/members', [TeamController::class, 'addMember'])->middleware('team.member');
+        Route::delete('/teams/{id}/members/{userId}', [TeamController::class, 'removeMember'])->middleware('team.member');
         
         // Task routes
         Route::post('/tasks', [TaskController::class, 'store']);
         Route::get('/tasks', [TaskController::class, 'index']);
-        Route::put('/tasks/{id}', [TaskController::class, 'update']);
-        Route::delete('/tasks/{id}', [TaskController::class, 'destroy']);
-        Route::post('/tasks/{id}/files', [TaskController::class, 'uploadFile']);
+        Route::put('/tasks/{id}', [TaskController::class, 'update'])->middleware('team.member');
+        Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->middleware('team.member');
+        Route::post('/tasks/{id}/files', [TaskController::class, 'uploadFile'])->middleware('team.member');
     });
 });
